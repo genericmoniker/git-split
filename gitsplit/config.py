c8fa2e3fd7cb4df8f4d93fd7ff2a8585d3f25af9
Eric Smith <eric@esmithy.net>
2020-01-22 19:51:04 -0700

    Added pyfakefs for testing; further implementation

--- gitsplit/config.py
+++ gitsplit/config.py
@@ -10,6 +10,11 @@ class Config:
     def __init__(self, config_data: str, base_path: Path):
         self._base_path = base_path
         data = toml.loads(config_data)
+        source = data.get("source")
+        if not source:
+            raise ConfigError("Source file not specified in the config file.")
+        self._source_file = SourceFile(base_path / source)
+
         self._split_files = [
             base_path / k
             for k in data.keys()
@@ -32,3 +37,38 @@ class Config:
 
 class ConfigError(Exception):
     """Exception raised for configuration errors."""
+
+
+class SourceFile:
+    """A source file to be split."""
+
+    def __init__(self, path: Path):
+        if not path.exists():
+            raise ConfigError(f'Source file "{path}" does not exist.')
+        self._path = path
+        self._line_count = None
+
+    @property
+    def line_count(self):
+        if self._line_count is None:
+            self._line_count = sum(1 for _ in self.lines)
+        return self._line_count
+
+    @property
+    def lines(self):
+        with self._path.open("r") as f:
+            yield from f
+
+
+class SplitFile:
+    """A target file for splitting into."""
+
+    def __init__(self, path: Path, lines: str):
+        self._path = path
+        self._lines = lines
+
+    def exists(self):
+        return self._path.exists()
+
+    def todo(self):
+        pass
--- test/gitsplit/config_test.py
+++ test/gitsplit/config_test.py
@@ -1,21 +1,49 @@
 from pathlib import Path
-from unittest import mock
 
 import pytest
 
-from gitsplit.config import Config, ConfigError
+from gitsplit.config import Config, ConfigError, SourceFile
 
 
-def test_config_error_if_no_split_files():
+def test_config_error_if_source_file_not_specified():
+    config_data = '[file]\nlines="1"'
+    with pytest.raises(ConfigError) as excinfo:
+        Config(config_data, base_path=Path("/"))
+    assert "Source" in str(excinfo.value)
+
+
+@pytest.mark.usefixtures("fs")
+def test_config_error_if_source_file_does_not_exist():
+    config_data = 'source="src"\n[file]\nlines="1"'
+    with pytest.raises(ConfigError) as excinfo:
+        Config(config_data, base_path=Path("/"))
+    assert "exist" in str(excinfo.value)
+
+
+def test_config_error_if_no_split_files(fs):
     config_data = 'source="src"'
+    fs.create_file("src")
     with pytest.raises(ConfigError) as excinfo:
         Config(config_data, base_path=Path("/"))
     assert "split files" in str(excinfo.value)
 
 
-def test_config_error_if_split_file_exists():
-    config_data = 'source="src"\n[exists]\nlines="1"'
-    with mock.patch.object(Path, "exists", return_value=True):
-        with pytest.raises(ConfigError) as excinfo:
-            Config(config_data, base_path=Path("/"))
-    assert "exists" in str(excinfo.value)
+@pytest.mark.xfail(reason="Not yet implemented.")
+def test_config_error_if_split_file_has_no_lines(fs):
+    config_data = 'source="src"\n[file]\n'
+    fs.create_file("src")
+    with pytest.raises(ConfigError) as excinfo:
+        Config(config_data, base_path=Path("/"))
+    assert "lines" in str(excinfo.value)
+
+
+def test_source_file_line_count(fs):
+    fs.create_file("src", contents="one \n two \n three \n")
+    source_file = SourceFile(Path("src"))
+    assert source_file.line_count == 3
+
+
+def test_source_file_lines(fs):
+    fs.create_file("src", contents="one \n two \n three \n")
+    source_file = SourceFile(Path("src"))
+    assert list(source_file.lines) == ["one \n", " two \n", " three \n"]
